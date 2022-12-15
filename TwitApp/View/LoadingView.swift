71f05636ab3a134b5c18b53b9eb55fb72824d957
Stanislav Sobolevski <minuel.colt@gmail.com>
2022-12-15 18:56:01 +0300

    add new View (LoadingView)

--- TwitApp/View/LoadingView.swift
+++ TwitApp/View/LoadingView.swift
@@ -7,14 +7,11 @@
 
 import SwiftUI
 
-struct LoaingView: View {
+struct LoadingView: View {
+    @Binding var show: Bool
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct LoaingView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoaingView()
-    }
-}
+
