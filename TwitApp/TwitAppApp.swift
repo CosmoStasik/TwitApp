64cfbf49d6b9567a531692da6944506b7a7dc757
Stanislav Sobolevski <minuel.colt@gmail.com>
2022-12-15 16:15:09 +0300

    Initial Commit

---
+++ TwitApp/TwitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitAppApp.swift
+//  TwitApp
+//
+//  Created by Stanislav Sobolevsky on 15.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TwitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
