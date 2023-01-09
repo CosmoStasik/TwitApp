ca363296dd24d5fc8287fa7073b9aa92b8d7e172
Stanislav Sobolevski <minuel.colt@gmail.com>
2023-01-09 16:50:57 +0300

    Add PostView

--- TwitApp/View/MainView/PostView/ReusablePostView.swift
+++ TwitApp/View/MainView/PostView/ReusablePostView.swift
@@ -11,9 +11,13 @@ import FirebaseFirestore
 
 
 struct ReusablePostView: View {
+    var basedOnUID: Bool = false
+    var uid: String = ""
     @Binding var posts: [Post]
     // View Properties
-    @State var isFetching: Bool = true
+    @State private var isFetching: Bool = true
+    // Pagination
+    @State private var paginationDoc: QueryDocumentSnapshot?
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             LazyVStack{
@@ -40,8 +44,12 @@ struct ReusablePostView: View {
         }
         .refreshable {
             // Scroll to Refresh
+            // Disbaling Refresh for UID based Posts
+            guard !basedOnUID else {return}
         isFetching = true
             posts = []
+            // Resetting Pagination Doc
+            paginationDoc = nil
             await fetchPosts() // т,к Выражиение асинхронно оно должен быть помеченно "ожиданием"
         }
         
@@ -69,6 +77,15 @@ struct ReusablePostView: View {
                     posts.removeAll{post.id == $0.id}
                 }
             }
+            .onAppear {
+                // When last Post Appears, Fetching New Post (If There)
+                ///Разбиение на тсраницы, проверяем пусто ли, тк на тсранице я указал видно 20 постов/или 20 абзацов, а значит
+                ///если будет 40, то переход на новую, если будет пусто, то не будет происходить лишнего поиска/загрузки(который может привести к крашу
+                if post.id == posts.last?.id && paginationDoc != nil {
+                    //print("Fetch New Post")
+                    Task{await fetchPosts()}
+                }
+            }
 
                Divider()
                 .padding(.horizontal,-15)
@@ -81,15 +98,31 @@ struct ReusablePostView: View {
     func fetchPosts()async{
         do {
             var query: Query!
-            query = Firestore.firestore().collection("Posts")
-                .order(by: "publishedDate", descending: true)
-                .limit(to: 20)
+            // Implementing Pagination
+            if let paginationDoc {
+                query = Firestore.firestore().collection("Posts")
+                    .order(by: "publishedDate", descending: true)
+                    .start(afterDocument: paginationDoc)
+                    .limit(to: 20) //limit post's on page
+            } else {
+                query = Firestore.firestore().collection("Posts")
+                    .order(by: "publishedDate", descending: true)
+                    .limit(to: 20)
+            }
+            // New Query for UID Based Document Fetch
+            // Simply Filter the Posts Which not belongs to this UID
+            if basedOnUID{
+                query = query
+                    .whereField("userUID", isEqualTo: uid)
+            }
+           
             let docs = try await query.getDocuments()
             let fetchedPosts = docs.documents.compactMap { doc -> Post? in
                 try? doc.data(as: Post.self)
             }
             await MainActor.run(body: {
-                posts = fetchedPosts
+                posts.append(contentsOf: fetchedPosts)
+                paginationDoc = docs.documents.last
                 isFetching = false
             })
         } catch {
---
+++ TwitApp/View/MainView/SearchUserView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchUserView.swift
+//  TwitApp
+//
+//  Created by Stanislav Sobolevsky on 29.12.22.
+//
+
+import SwiftUI
+
+struct SearchUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchUserView()
+    }
+}
